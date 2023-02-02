7ee692fbff6967b79c98c98249ddb720c07cecf3
sini-mary <marysnaidaa@gmail.com>
2023-02-02 09:10:14 +0300

    message

---
+++ Main.kt
@@ -0,0 +1,10 @@
+bbfun main(args: Array<String>) {
+    var full_name = "Mary Snaida"
+    var age = 21
+    var phone_number = 731865465
+    var weight_InKgs = 55
+    var citizenshipkenyan = true
+    var citizenshipnotkenyan = false
+
+
+}
